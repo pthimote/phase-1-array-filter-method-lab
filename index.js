0cceb30779c81c43085855a3a1435d8d7b17c721
Paul Thimote <pthimote@gmail.com>
2022-09-14 21:06:12 -0400

    commit tree 7c4fedab0947f7111bb2cfe4b8eeedfcbef445ac

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(array, driver) {
+    const drivers = array.filter(match => match.toUpperCase() === driver.toUpperCase())
+    return drivers
+}
+
+function fuzzyMatch(array, driver) {
+    const drivers = array.filter(drivername => drivername.toLowerCase().indexOf(driver.toLowerCase())=== 0)
+    return drivers
+}
+
+function matchName(array, string) {
+    const drivers = array.filter(driver => driver.name == string)
+    return drivers
+}
